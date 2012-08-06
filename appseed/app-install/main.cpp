c940a69f1e8a2f99ac198a91f03a75c9dc2fedd6
ca2 <camilo@ca2.email>
2012-08-06 23:26:11 +0000

    Eu te amo Thommy, Thomas Borregaard Sørensen!!

--- appseed/app-install/main.cpp
+++ appseed/app-install/main.cpp
@@ -80,7 +80,7 @@ extern bool g_bInstalling;
 
 installer::installer()
 {
-
+   //debug_box("app-install", "app", 0);
    m_hinstance             = ::GetModuleHandleA(NULL);
    m_hmutexSpabootInstall  = NULL;
    e_message m_emessage    = message_none;
