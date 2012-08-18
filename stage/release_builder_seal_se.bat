aba9459c227847ac5a1b2438f7704bf5dd331550
ca2 basis release builder <basis_relbuilder@ca2.cc>
2012-08-18 15:17:15 +0000

    ﻿ at votagus/telmico production stage folder V:/stage/app 18/08/2012 12:17  at loft7653 at ServerLoft - serverloft.com - Fujitsu PRIMERGY RX100 S6 Intel(R) Xeon(R) X3440 @ 2.53GHz Quadcore 8 threads processor 8.0GB DDR3-ECC RAM 1TB RAID1 HDD SATA storage 60GB RAID1 SSD OCZ Vertex 2 storage high-speed Internet Windows Web Server 2008 R2 64-bit Service Pack 1 at Strausbourg, France/Germany.﻿
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- stage/release_builder_seal_se.bat
+++ stage/release_builder_seal_se.bat
@@ -1,6 +1,6 @@
-type V:\basis\nodeapp\stage\build_folder_pp_comment_se.txt > V:\basis\nodeapp\stage\release_builder_seal_se.txt
+type V:\basis\app\stage\build_folder_pp_comment_se.txt > V:\basis\nodeapp\stage\release_builder_seal_se.txt
 date /T >> V:\basis\nodeapp\stage\release_builder_seal_se.txt
 time /T >> V:\basis\nodeapp\stage\release_builder_seal_se.txt
-type V:\basis\nodeapp\stage\build_machine_pp_comment_se.txt >> V:\basis\nodeapp\stage\release_builder_seal_se.txt
+type V:\basis\app\stage\build_machine_pp_comment_se.txt >> V:\basis\nodeapp\stage\release_builder_seal_se.txt
 type G:\cecyn1.at.hotmail.com\carlos\dedicaverse.txt >> V:\basis\nodeapp\stage\release_builder_seal_se.txt
-svn commit --force-log --encoding utf-8 --file=V:\basis\nodeapp\stage\release_builder_seal_se.txt V:\basis\nodeapp\stage
+svn commit --force-log --encoding utf-8 --file=V:\basis\nodeapp\stage\release_builder_seal_se.txt V:\basis\nodeapp
