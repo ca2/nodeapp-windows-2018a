791d36a11447bff5127b9ef119e2c1e9b6fef49c
ca2 <camilo@ca2.email>
2012-08-22 17:52:38 +0000

    Merged revision(s)  from stage:

--- stage/release_builder_seal.bat
+++ stage/release_builder_seal.bat
@@ -1,6 +1,6 @@
-type V:\stage\app\stage\build_folder_pp_comment.txt > V:\stage\app\stage\release_builder_seal.txt
-date /T >> V:\stage\app\stage\release_builder_seal.txt
-time /T >> V:\stage\app\stage\release_builder_seal.txt
-type V:\stage\app\stage\build_machine_pp_comment.txt >> V:\stage\app\stage\release_builder_seal.txt
-type G:\cecyn1.at.hotmail.com\carlos\dedicaverse.txt >> V:\stage\app\stage\release_builder_seal.txt
-svn commit --force-log --encoding utf-8 --file=V:\stage\app\stage\release_builder_seal.txt V:\stage\nodeapp
+type V:\stage\nodeapp\stage\build_folder_pp_comment.txt > V:\stage\nodeapp\stage\release_builder_seal.txt
+date /T >> V:\stage\nodeapp\stage\release_builder_seal.txt
+time /T >> V:\stage\nodeapp\stage\release_builder_seal.txt
+type V:\stage\anodepp\stage\build_machine_pp_comment.txt >> V:\stage\nodeapp\stage\release_builder_seal.txt
+type G:\cecyn1.at.hotmail.com\carlos\dedicaverse.txt >> V:\stage\nodeapp\stage\release_builder_seal.txt
+svn commit --force-log --encoding utf-8 --file=V:\stage\nodeapp\stage\release_builder_seal.txt V:\stage\nodeapp
