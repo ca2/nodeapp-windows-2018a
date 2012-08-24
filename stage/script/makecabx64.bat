60e52fac01e247ad831956e7f7c3b6e835ec5ec3
ca2 basis release builder <basis_relbuilder@ca2.cc>
2012-08-24 09:01:53 +0000

    ﻿ at votagus/telmico production stage folder V:/stage/app 24/08/2012 06:01  at loft7653 at ServerLoft - serverloft.com - Fujitsu PRIMERGY RX100 S6 Intel(R) Xeon(R) X3440 @ 2.53GHz Quadcore 8 threads processor 8.0GB DDR3-ECC RAM 1TB RAID1 HDD SATA storage 60GB RAID1 SSD OCZ Vertex 2 storage high-speed Internet Windows Web Server 2008 R2 64-bit Service Pack 1 at Strausbourg, France/Germany.﻿
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- stage/script/makecabx64.bat
+++ stage/script/makecabx64.bat
@@ -3,6 +3,6 @@ mkdir ..\..\..\time\iexca2\
 mkdir ..\..\..\time\iexca2\x64
 del ..\..\..\time\iexca2\x64\iexca2.cab
 IF NOT EXIST ..\..\..\time\iexca2\x64\iexca2.cab ECHO Deleted iexca2.cab
-..\..\..\app\thirdparty\binary\cabarc N ..\..\..\time\iexca2\x64\iexca2.cab iexca2.inf ..\..\..\stage\x64\iexca2.dll
+..\..\..\nodeapp\thirdparty\binary\cabarc N ..\..\..\time\iexca2\x64\iexca2.cab iexca2.inf ..\..\..\stage\x64\iexca2.dll
 IF NOT EXIST ..\..\..\time\iexca2\x64\iexca2.cab ECHO ERROR WAS HAPPEND!!! 
 IF EXIST ..\..\..\time\iexca2\x64\iexca2.cab ECHO Copy was successfully
