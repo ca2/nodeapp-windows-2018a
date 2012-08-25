9e24cdd615c0d907a281724e7ce1ef901f752678
ca2 <camilo@ca2.email>
2012-08-25 21:56:01 +0000

    Eu te amo Thommy, Thomas Borregaard Sørensen!!
    Feliz 61 anos, Mummi!!
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- stage/install/basis/x86/ja-jp - laboratory/core_deepfish_alatel.bat
+++ stage/install/basis/x86/ja-jp - laboratory/core_deepfish_alatel.bat
@@ -1,2 +1,2 @@
 call _setup
-app : app=app-core/deepfish_alatel build_number=basis install
+app : app=app-core/deepfish_alatel build_number=basis locale=ja-jp schema=laboratory install
--- stage/install/basis/x86/ja-jp - laboratory/core_uinteraction.bat
+++ stage/install/basis/x86/ja-jp - laboratory/core_uinteraction.bat
@@ -1,2 +1,2 @@
 call _setup
-app : app=app-core/uinteraction build_number=basis install
+app : app=app-core/uinteraction build_number=basis locale=ja-jp schema=laboratory install
