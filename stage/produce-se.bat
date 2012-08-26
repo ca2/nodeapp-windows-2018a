09e942d98f3df6459f1c91b232fb5e2496dd0a15
ca2 basis release builder <basis_relbuilder@ca2.cc>
2012-08-26 19:55:31 +0000

    ﻿ at votagus/telmico production stage folder V:/stage/app 26/08/2012 16:56  at loft7653 at ServerLoft - serverloft.com - Fujitsu PRIMERGY RX100 S6 Intel(R) Xeon(R) X3440 @ 2.53GHz Quadcore 8 threads processor 8.0GB DDR3-ECC RAM 1TB RAID1 HDD SATA storage 60GB RAID1 SSD OCZ Vertex 2 storage high-speed Internet Windows Web Server 2008 R2 64-bit Service Pack 1 at Strausbourg, France/Germany.

--- stage/produce-se.bat
+++ stage/produce-se.bat
@@ -1,4 +1,4 @@
-@echo going to telmico/votagus production folder
-V:
+@echo going to production folder
+Z:
 cd \production\stage\x64\
-app.exe : app=nodeapp/production start_deferred=1 base_dir=V:\basis version=basis build_number=basis
+app.exe : app=nodeapp/production start_deferred=1 base_dir=Z:\basis version=basis build_number=basis
