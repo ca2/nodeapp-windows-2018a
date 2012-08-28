69d07672a6a54a916c32710cbb29fcc6b4348ed0
ca2 basis release builder <basis_relbuilder@ca2.cc>
2012-08-28 17:00:23 +0000

    ﻿ at votagus/telmico production stage folder V:/stage/app 28/08/2012 14:01  at loft7653 at ServerLoft - serverloft.com - Fujitsu PRIMERGY RX100 S6 Intel(R) Xeon(R) X3440 @ 2.53GHz Quadcore 8 threads processor 8.0GB DDR3-ECC RAM 1TB RAID1 HDD SATA storage 60GB RAID1 SSD OCZ Vertex 2 storage high-speed Internet Windows Web Server 2008 R2 64-bit Service Pack 1 at Strausbourg, France/Germany.

--- stage/script/makecabx64.bat
+++ stage/script/makecabx64.bat
@@ -2,7 +2,14 @@
 mkdir ..\..\..\time\iexca2\
 mkdir ..\..\..\time\iexca2\x64
 del ..\..\..\time\iexca2\x64\iexca2.cab
+del ..\..\..\time\iexca2\x64\iexca2.dll
+del ..\..\time\iexca2\x64\app-install.exe
+copy ..\..\..\stage\x64\iexca2.dll ..\..\..\time\iexca2\x64\iexca2.dll
+copy ..\..\..\stage\x64\app-install.exe ..\..\..\time\iexca2\x64\app-install.exe
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\iexca2.dll
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\app-install.exe
 IF NOT EXIST ..\..\..\time\iexca2\x64\iexca2.cab ECHO Deleted iexca2.cab
-..\..\..\nodeapp\thirdparty\binary\cabarc N ..\..\..\time\iexca2\x64\iexca2.cab iexca2.inf ..\..\..\stage\x64\iexca2.dll
+..\..\..\nodeapp\thirdparty\binary\cabarc N ..\..\..\time\iexca2\x64\iexca2.cab iexca2.inf ..\..\..\time\iexca2\x64\iexca2.dll ..\..\..\time\iexca2\x64\app-install.exe
 IF NOT EXIST ..\..\..\time\iexca2\x64\iexca2.cab ECHO ERROR WAS HAPPEND!!! 
-IF EXIST ..\..\..\time\iexca2\x64\iexca2.cab ECHO Copy was successfully
+IF EXIST ;.\..\..\time\iexca2\x64\iexca2.cab ECHO Copy was successfully
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\iexca2.cab
--- stage/script/makecabx86.bat
+++ stage/script/makecabx86.bat
@@ -6,10 +6,10 @@ del ..\..\..\time\iexca2\x86\iexca2.dll
 del ..\..\time\iexca2\x86\app-install.exe
 copy ..\..\..\stage\x86\iexca2.dll ..\..\..\time\iexca2\x86\iexca2.dll
 copy ..\..\..\stage\x86\app-install.exe ..\..\..\time\iexca2\x86\app-install.exe
-..\..\..\nodeapp\thirdparty\binary\signtool sign /f "A:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\iexca2.dll
-..\..\..\nodeapp\thirdparty\binary\signtool sign /f "A:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\app-install.exe
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\iexca2.dll
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\app-install.exe
 IF NOT EXIST ..\..\..\time\iexca2\x86\iexca2.cab ECHO Deleted iexca2.cab
 ..\..\..\nodeapp\thirdparty\binary\cabarc N ..\..\..\time\iexca2\x86\iexca2.cab iexca2.inf ..\..\..\time\iexca2\x86\iexca2.dll ..\..\..\time\iexca2\x86\app-install.exe
 IF NOT EXIST ..\..\..\time\iexca2\x86\iexca2.cab ECHO ERROR WAS HAPPEND!!! 
 IF EXIST ;.\..\..\time\iexca2\x86\iexca2.cab ECHO Copy was successfully
-..\..\..\nodeapp\thirdparty\binary\signtool sign /f "A:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\iexca2.cab
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\iexca2.cab
