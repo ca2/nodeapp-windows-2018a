af69cea87d2e5487568d34f59f9c77ce212255c3
ca2 <camilo@ca2.email>
2012-10-03 13:08:08 +0000

    Merged revision(s)  from stage: I love you so much, Thommy, Thomas Borregaard Sørensen!! at cecyn1 - Cgcl - app folder Z:\ca2\app - at camilox64w7 at xpressions.net - at Dell Optiplex 780 Intel(R) Core(TM)2 Duo CPU E8500 @ 3.16GHz 7GB RAM 500GB + 240GB storage with Windows 7 Professional 64-bit Service Pack 1 - with English - United States - Language for non-Unicode programs - ~512kbits/sec Internet at sector 8J at CITS for Cycos/Siemens
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- stage/release_builder_seal.bat
+++ stage/release_builder_seal.bat
@@ -1,5 +1,5 @@
 type Z:\stage\nodeapp\stage\build_folder_pp_comment.txt > Z:\stage\nodeapp\stage\release_builder_seal.txt
 date /T >> Z:\stage\nodeapp\stage\release_builder_seal.txt
 time /T >> Z:\stage\nodeapp\stage\release_builder_seal.txt
-type Z:\stage\anodepp\stage\build_machine_pp_comment.txt >> Z:\stage\nodeapp\stage\release_builder_seal.txt
+type Z:\stage\nodepp\stage\build_machine_pp_comment.txt >> Z:\stage\nodeapp\stage\release_builder_seal.txt
 svn commit --force-log --encoding utf-8 --file=Z:\stage\nodeapp\stage\release_builder_seal.txt Z:\stage\nodeapp
