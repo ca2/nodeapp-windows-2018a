6815940ffc624cd3d1b22f713d5c5d922bf083cd
ca2 <camilo@ca2.email>
2012-10-03 08:58:32 +0000

    Revisão(ões)  de stage combinada(s): Eu te amo Thommy, Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    
    at inha at veriverse - at Dell XPS 8300 Intel(R) Core(TM) i7-2600 CPU @ 3.40GHz 16GB RAM 500GB + 500GB 7200rpm storage + 120 GB SSD OCZ Agility 3 with Windows 7 Ultimate 64-bit Service Pack 1 - with Portuguese - Brazil - Language for non-Unicode programs - ~10Mpbs NET Virtua Internet at Alameda Princesa Izabel, 3000 - Curitiba - PR - Brazil
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- stage/release_builder_seal.bat
+++ stage/release_builder_seal.bat
@@ -1,6 +1,5 @@
-type V:\stage\nodeapp\stage\build_folder_pp_comment.txt > V:\stage\nodeapp\stage\release_builder_seal.txt
-date /T >> V:\stage\nodeapp\stage\release_builder_seal.txt
-time /T >> V:\stage\nodeapp\stage\release_builder_seal.txt
-type V:\stage\anodepp\stage\build_machine_pp_comment.txt >> V:\stage\nodeapp\stage\release_builder_seal.txt
-type G:\cecyn1.at.hotmail.com\carlos\dedicaverse.txt >> V:\stage\nodeapp\stage\release_builder_seal.txt
-svn commit --force-log --encoding utf-8 --file=V:\stage\nodeapp\stage\release_builder_seal.txt V:\stage\nodeapp
+type Z:\stage\nodeapp\stage\build_folder_pp_comment.txt > Z:\stage\nodeapp\stage\release_builder_seal.txt
+date /T >> Z:\stage\nodeapp\stage\release_builder_seal.txt
+time /T >> Z:\stage\nodeapp\stage\release_builder_seal.txt
+type Z:\stage\anodepp\stage\build_machine_pp_comment.txt >> Z:\stage\nodeapp\stage\release_builder_seal.txt
+svn commit --force-log --encoding utf-8 --file=Z:\stage\nodeapp\stage\release_builder_seal.txt Z:\stage\nodeapp
