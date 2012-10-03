a990266018f228351e9c081f96d27b0768dd9e98
ca2 <camilo@ca2.email>
2012-10-03 08:43:48 +0000

    Revisão(ões)  de stage combinada(s): Eu te amo Thommy, Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    
    at inha at veriverse - at Dell XPS 8300 Intel(R) Core(TM) i7-2600 CPU @ 3.40GHz 16GB RAM 500GB + 500GB 7200rpm storage + 120 GB SSD OCZ Agility 3 with Windows 7 Ultimate 64-bit Service Pack 1 - with Portuguese - Brazil - Language for non-Unicode programs - ~10Mpbs NET Virtua Internet at Alameda Princesa Izabel, 3000 - Curitiba - PR - Brazil
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- stage/produce-us.bat
+++ stage/produce-us.bat
@@ -1,4 +1,4 @@
-@echo going to telmico/votagus production folder
-V:
+@echo going to production folder
+Z:
 cd \production\stage\x64\
-app.exe : app=nodeapp/production start_deferred=1 base_dir=V:\stage version=stage build_number=stage_production_77
+app.exe : app=nodeapp/production start_deferred=1 base_dir=Z:\stage version=stage build_number=basis
