06dac09de29ea9c0f48c96f122e2083f42de299f
ca2 basis release builder <basis_relbuilder@ca2.cc>
2012-10-10 05:18:26 +0000

    ﻿basis production10/10/2012 02:18 ns4004035 ovh.pt - ovh breed Sandy Bridge Intel Core i3 2130 2x2(HT)x3.4+ GHz 16GB DDR3 RAM 2 x 1TB RAID1 HDD SATA2 storage high-speed Internet Windows 7 Ultimate SP1 890GB Soft-RAID1 Virtual Disk VMware ESXi 5 Update 1 Strausbourg, France/Germany.

--- stage/produce-us.bat
+++ stage/produce-us.bat
@@ -1,4 +1,4 @@
 @echo going to production folder
 Z:
 cd \production\stage\x64\
-app.exe : app=nodeapp/production start_deferred=1 base_dir=Z:\stage version=stage build_number=basis
+app.exe : app=nodeapp/production start_deferred=1 base_dir=Z:\stage version=stage build_number=basis quit_on_finish
