36385a2575c11301eac6f704ae5997068b13fda3
ca2 <camilo@ca2.email>
2012-12-13 11:16:52 +0000

    Eu te amo Thommy Gustavinho Cecynzinho Lundgrenzinho!! Eu te amo Thomas!!
    Mummi contribution!!!! Feliz 61 anos!!
    
    at ns4006502.ip-142-4-215.net at OVH US/CA/North America - through ovh.pt - OVH Made Intel Xeon E3 1245v2 @ 3.40GHz Turbo Boost 3.80Ghz Quadcore processor 8-Threads 32.0GB 2TB RAID1 high-speed 100MB Internet Premium Both Up/Down ESXi Host with at guest basis running Windows 8 Pro 14GB RAM 890GB Storage at BHS/Canada

--- os/win_window.cpp
+++ os/win_window.cpp
@@ -3154,8 +3154,8 @@ restart_mouse_hover_check:
    void window::_001OnProdevianSynch(gen::signal_object * pobj)
    {
       UNREFERENCED_PARAMETER(pobj);
-      System.get_event(m_pthread)->SetEvent();
-      System.get_event(System.get_twf())->wait(millis(8400));
+//      System.get_event(m_pthread)->SetEvent();
+  //    System.get_event(System.get_twf())->wait(millis(8400));
    }
 
    void window::_001OnPaint(gen::signal_object * pobj)
