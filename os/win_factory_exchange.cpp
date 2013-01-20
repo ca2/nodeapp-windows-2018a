e7db62b386e85de88a9a21fade05b62f8cb583d4
ca2 <camilo@ca2.email>
2013-01-20 22:45:07 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy Gustavo Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- os/win_factory_exchange.cpp
+++ os/win_factory_exchange.cpp
@@ -8,10 +8,11 @@ namespace win
    factory_exchange::factory_exchange(::ca::application * papp) :
       ca(papp)
    {
-      System.factory().cloneable_large <stdio_file          >  (System.template type_info < ::ex1::text_file         > ());
-      System.factory().cloneable_large < file               >  (System.template type_info < ::ex1::file              > ());
-      System.factory().cloneable_large < file_set            >  (System.template type_info < ::ex2::file_set          > ());
-      System.factory().cloneable < file_system               >  (System.template type_info < ::ca::file::system       > (), 1);
+
+      System.factory().cloneable_large <stdio_file             >  (System.template type_info < ::ex1::text_file         > ());
+      System.factory().cloneable_large < file                  >  (System.template type_info < ::ex1::file              > ());
+      System.factory().cloneable_large < file_set              >  (System.template type_info < ::ex2::file_set          > ());
+      System.factory().cloneable < file_system                 >  (System.template type_info < ::ca::file::system       > (), 1);
       System.factory().cloneable < WinResource                 >  (System.template type_info < ::ex1::resource          > (), 1);
       System.factory().cloneable < application                 >  (System.template type_info < ::ex2::application       > (), 1);
       System.factory().cloneable < dir                         >  (System.template type_info < ::ca::dir::system        > (), 1);
@@ -28,13 +29,14 @@ namespace win
       System.factory().creatable < window_draw                 >  (System.template type_info < ::ca::window_draw        > (), 1);
       System.factory().creatable_large < thread                >  (System.template type_info < ::ca::thread             > ());
       System.factory().creatable_large < window                >  (System.template type_info < ::ca::window             > ());
-      System.factory().cloneable_small < draw_dib              >  (System.template type_info < ::ca::draw_dib           > ());
       System.factory().creatable < os                          >  (System.template type_info < ::ca::os                 > (), 1);
       System.factory().creatable < port_forward                >  (System.template type_info < ::ca4::port_forward      > (), 1);
       System.factory().creatable_small < copydesk              >  (System.template type_info < ::ca4::copydesk          > (), 1);
       System.factory().creatable_small < crypt                 >  (System.template type_info < ::ca4::crypt             > (), 1);
       System.factory().creatable_small < ip_enum               >  (System.template type_info < ::ca4::ip_enum           > ());
+      
       ::win::dib::s_initialize();
+
    }
 
 
