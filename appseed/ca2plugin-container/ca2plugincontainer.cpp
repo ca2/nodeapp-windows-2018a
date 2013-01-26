9125d5ed224be8bfc2acc690ac95096ab53de0ca
ca2 <camilo@ca2.email>
2013-01-26 20:20:35 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy Gustavo Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- appseed/ca2plugin-container/ca2plugincontainer.cpp
+++ appseed/ca2plugin-container/ca2plugincontainer.cpp
@@ -34,13 +34,10 @@ int32_t __win_main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLin
 	
    UNREFERENCED_PARAMETER(lpCmdLine);
 
-
    ::CoInitialize(NULL);
-
 
    if(!main_initialize())
       return -1;
-
 
    //_set_purecall_handler(_ca2_purecall);
 
@@ -50,19 +47,18 @@ int32_t __win_main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLin
 
    int32_t nReturnCode = 0;
 
-
    vsstring * pstrChannel = new vsstring(get_command_line_param(::GetCommandLine(), "channel"));
 
-
    g_hmutex = ::CreateMutex(NULL, FALSE, "Global\\ca2::fontopus::ca2plugin-container::" + *pstrChannel);
+
    if(::GetLastError() == ERROR_ALREADY_EXISTS)
    {
+
       return -1;
+
    }
 
-
    start_thread(&thread_proc_app, (LPVOID) pstrChannel);
-
 
    MSG msg;
       
@@ -76,86 +72,123 @@ int32_t __win_main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLin
          break;
 
 		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+
+      DispatchMessage(&msg);
 
 	}
 
-
-
-
    try
    {
+
       main_finalize();
+
    }
    catch(...)
    {
+
    }
 
    try
    {
+
       //__win_term();
+
    }
    catch(...)
    {
+
    }
 
 
    try
    {
+
       //delete psystem;
+ 
    }
    catch(...)
    {
+   
    }
 
+   
    //psystem = NULL;
 
-
    try
    {
-//      delete __get_module_state()->m_pmapHWND;
+
+      //delete __get_module_state()->m_pmapHWND;
+
    }
    catch(...)
    {
+
    }
-/*   try
+
+/* 
+   try
    {
+
       delete __get_module_state()->m_pmapHDC;
+
    }
    catch(...)
    {
-   }*/
-/*   try
+
+   }
+*/
+
+/*
+   try
    {
+
       delete __get_module_state()->m_pmapHGDIOBJ;
+
    }
    catch(...)
    {
-   }*/
-//      delete __get_module_state()->m_pmapHMENU;
+
+   }
+*/
+
+   //delete __get_module_state()->m_pmapHMENU;
 
    try
    {
-//      __get_module_state()->m_pmapHWND     = NULL;
-   }
-   catch(...)
-   {
-   }
-/*   try
-   {
-      __get_module_state()->m_pmapHDC      = NULL;
-   }
-   catch(...)
-   {
-   }*/
-/*   try
-   {
-      __get_module_state()->m_pmapHGDIOBJ  = NULL;
-   }
-   catch(...)
-   {
-   }*/
 
+      //__get_module_state()->m_pmapHWND     = NULL;
+
+   }
+   catch(...)
+   {
+
+   }
+
+/* 
+   try
+   {
+
+      __get_module_state()->m_pmapHDC      = NULL;
+
+   }
+   catch(...)
+   {
+
+   }
+*/
+
+/* 
+   try
+   {
+
+      __get_module_state()->m_pmapHGDIOBJ  = NULL;
+
+   }
+   catch(...)
+   {
+
+   }
+*/
 
    set_heap_mutex(NULL);
 
