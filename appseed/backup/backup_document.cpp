f32e13ce71e7bc2ddd68ee73be6ac426667aefbe
ca2 <camilo@ca2.email>
2013-10-01 18:02:19 +0000

    Eu te amo Thommy Gustavo Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 62 anos!! Parabéns!!
    
    ｛
    ｝
    
                            -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- appseed/backup/backup_document.cpp
+++ appseed/backup/backup_document.cpp
@@ -7,7 +7,7 @@ namespace backup
 
    document::document(sp(base_application) papp) :
       element(papp),
-      ::ca2::data_container_base(papp),
+      ::data::data_container_base(papp),
       production::document(papp)
    {
 
