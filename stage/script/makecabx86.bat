56e9bce04c2322c48e1b578e575ebb461b42012b
ca2 <camilo@ca2.email>
2013-12-05 21:43:47 +0000

    -c1-production -c2-producer -t12n-producing -mmmi- 2013-12-05 21-42-29 app:SVN7107, app-cidadedecuritiba:SVN236, app-core:SVN2012, app-frontpage:SVN213, app-sysutils:SVN145, app-veriwell:SVN1104, nodeapp:SVN1469M -  at usloft2405 at ServerLoft - serverloft.com - Fujistu 2 processors Intel(R) Xeon(R) E5520 @ 2.27GHz Processors - 16 cores - 12.0GB RAM 1TB RAID1 HDD storage 60GB RAID1 SSD storage high-speed Internet Windows Server 2008 R2 Standard 64-bit Service Pack 1 at Saint Louis, Missouri - United States, North America, this machine and telmo to speed up effectiveness, performance and build funcionality and readiness!!!!!!!!#define THIS_PRODUCT_VERSION "2013-12-05 21-42-29 app:SVN7107, app-cidadedecuritiba:SVN236, app-core:SVN2012, app-frontpage:SVN213, app-sysutils:SVN145, app-veriwell:SVN1104, nodeapp:SVN1469M\0" #define THIS_FILE_VERSION "2013-12-05 21-42-29 app:SVN7107, app-cidadedecuritiba:SVN236, app-core:SVN2012, app-frontpage:SVN213, app-sysutils:SVN145, app-veriwell:SVN1104, nodeapp:SVN1469M\0" #define __THIS_PRODUCT_VERSION 2013,1205,2142,29 #define __THIS_FILE_VERSION 2013,1205,2142,29

--- stage/script/makecabx86.bat
+++ stage/script/makecabx86.bat
@@ -3,13 +3,16 @@ mkdir ..\..\..\time\iexca2\
 mkdir ..\..\..\time\iexca2\x86
 del ..\..\..\time\iexca2\x86\iexca2.cab
 del ..\..\..\time\iexca2\x86\iexca2.dll
+del ..\..\..\time\iexca2\x86\base.dll
 del ..\..\time\iexca2\x86\app-install.exe
 copy ..\..\..\stage\x86\iexca2.dll ..\..\..\time\iexca2\x86\iexca2.dll
+copy ..\..\..\stage\x86\base.dll ..\..\..\time\iexca2\x86\base.dll
 copy ..\..\..\stage\x86\app-install.exe ..\..\..\time\iexca2\x86\app-install.exe
 ..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\iexca2.dll
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\base.dll
 ..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\app-install.exe
 IF NOT EXIST ..\..\..\time\iexca2\x86\iexca2.cab ECHO Deleted iexca2.cab
-..\..\..\nodeapp\thirdparty\binary\cabarc N ..\..\..\time\iexca2\x86\iexca2.cab iexca2.inf ..\..\..\time\iexca2\x86\iexca2.dll ..\..\..\time\iexca2\x86\app-install.exe
+..\..\..\nodeapp\thirdparty\binary\cabarc N ..\..\..\time\iexca2\x86\iexca2.cab iexca2.inf ..\..\..\time\iexca2\x86\base.dll ..\..\..\time\iexca2\x86\iexca2.dll ..\..\..\time\iexca2\x86\app-install.exe
 IF NOT EXIST ..\..\..\time\iexca2\x86\iexca2.cab ECHO ERROR WAS HAPPEND!!! 
 IF EXIST ;.\..\..\time\iexca2\x86\iexca2.cab ECHO Copy was successfully
 ..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\iexca2.cab
