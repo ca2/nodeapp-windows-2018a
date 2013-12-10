0418d436f9b81f9e6eef260a8195f77803f6bb67
ca2 <camilo@ca2.email>
2013-12-10 07:17:00 +0000

    -c1-production -c2-producer -t12n-producing -mmmi- 2013-12-10 07-06-58 app:SVN7131:7132, app-cidadedecuritiba:SVN238, app-core:SVN2022:2023, app-frontpage:SVN215, app-sysutils:SVN147, app-veriwell:SVN1107, nodeapp:SVN1480:1481M -  at usloft2405 at ServerLoft - serverloft.com - Fujistu 2 processors Intel(R) Xeon(R) E5520 @ 2.27GHz Processors - 16 cores - 12.0GB RAM 1TB RAID1 HDD storage 60GB RAID1 SSD storage high-speed Internet Windows Server 2008 R2 Standard 64-bit Service Pack 1 at Saint Louis, Missouri - United States, North America, this machine and telmo to speed up effectiveness, performance and build funcionality and readiness!!!!!!!!#define THIS_PRODUCT_VERSION "2013-12-10 07-06-58 app:SVN7131:7132, app-cidadedecuritiba:SVN238, app-core:SVN2022:2023, app-frontpage:SVN215, app-sysutils:SVN147, app-veriwell:SVN1107, nodeapp:SVN1480:1481M\0" #define THIS_FILE_VERSION "2013-12-10 07-06-58 app:SVN7131:7132, app-cidadedecuritiba:SVN238, app-core:SVN2022:2023, app-frontpage:SVN215, app-sysutils:SVN147, app-veriwell:SVN1107, nodeapp:SVN1480:1481M\0" #define __THIS_PRODUCT_VERSION 2013,1210,0706,58 #define __THIS_FILE_VERSION 2013,1210,0706,58

--- stage/script/makecabx86.bat
+++ stage/script/makecabx86.bat
@@ -18,7 +18,7 @@ copy ..\..\..\stage\x86\app-install.exe ..\..\..\time\iexca2\x86\app-install.exe
 ..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\draw2d_gdiplus.dll
 ..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\app-install.exe
 IF NOT EXIST ..\..\..\time\iexca2\x86\iexca2.cab ECHO Deleted iexca2.cab
-..\..\..\nodeapp\thirdparty\binary\cabarc N ..\..\..\time\iexca2\x86\iexca2.cab iexca2.inf ..\..\..\time\iexca2\x86\draw2d_gdipluse.dll ..\..\..\time\iexca2\x86\os.dll ..\..\..\time\iexca2\x86\base.dll ..\..\..\time\iexca2\x86\iexca2.dll ..\..\..\time\iexca2\x86\app-install.exe
+..\..\..\nodeapp\thirdparty\binary\cabarc N ..\..\..\time\iexca2\x86\iexca2.cab iexca2.inf ..\..\..\time\iexca2\x86\draw2d_gdiplus.dll ..\..\..\time\iexca2\x86\os.dll ..\..\..\time\iexca2\x86\base.dll ..\..\..\time\iexca2\x86\iexca2.dll ..\..\..\time\iexca2\x86\app-install.exe
 IF NOT EXIST ..\..\..\time\iexca2\x86\iexca2.cab ECHO ERROR WAS HAPPEND!!! 
 IF EXIST ;.\..\..\time\iexca2\x86\iexca2.cab ECHO Copy was successfully
 ..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x86\iexca2.cab
