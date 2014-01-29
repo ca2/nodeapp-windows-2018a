e6d83a2ba4243035a398720a82ab371c67b099d3
ca2 <camilo@ca2.email>
2014-01-29 15:16:44 +0000

    Welcome 2014!! from Curitiba back again!! Eu te amo Thomas Borregaard Sørensen!!
    Eu te amo Mummi!! Feliz 62 anos!!
    
    at inha at veriverse - at Dell XPS 8300 Intel(R) Core(TM) i7-2600 CPU 4 core 8 threads @ 3.40GHz 16GB RAM 500GB + 500GB 7200rpm storage + 120 GB SSD OCZ Agility 3 with Windows 8 Pro - with Portuguese - Brazil- 10Mpbs Download and 2Mpbs Upload NET Virtua Internet with LG 23´´ monitor, Microsoft Comfort Curve 2000 v1.0 Wired Keyboard and Microsoft Comfort Optical Wired Mouse 3000 at Alameda Princesa Izabel, 3000 - Curitiba - PR - Brazil
    "Z:\ca2\" folder
    
    ｛
    ｝
    
       ----
                            -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- operational_system/get_platform.h
+++ operational_system/get_platform.h
@@ -54,8 +54,8 @@ END_EXTERN_C
 #endif
 
 
-
-#define _WIN32_WINNT    0x0601 // _WIN32_WINNT_WIN7
+#define _WIN32_WINNT 0x0600 // _WIN32_WINNT_LONGHORN
+//#define _WIN32_WINNT    0x0601 // _WIN32_WINNT_WIN7
 #define _WIN32_IE       0x0800 // _WIN32_IE_IE80
 #define WINVER          _WIN32_WINNT
 
