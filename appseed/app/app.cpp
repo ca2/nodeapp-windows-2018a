bdd670f14c3cebf330f0291324e1f8f11e611496
ca2 <camilo@ca2.email>
2014-05-22 19:59:41 +0000

    Mummi contribution to ca2 Desenvolvimento de Software self-image.
    Eu te amo Thommy Gustavo Borregaard Sørensen!!
    <3JegElskerDigThommy!!
    Mummi contribution!!!! Feliz 63 anos!! Graças a Deus!! Parabéns!!
    
    at inha at veriverse at ca2!!
    Dell XPS 8300 with Intel(R) Core(TM) i7-2600 CPU @ 3.4GHz 4 cores 8 threads 16GB RAM 500 GB x 2 HDD storage, 120 GB SSD OCZ Storage, 1TB x 2 Samsung Story Station Backup and Support HDD External Storage Windows 8.1 Pro at Alameda Princesa Izabel, 3000 Bigorrilho Curitiba PR Brazil
    from "Z:\ca2\" folder at inha-veriverse!!
    
    Remotely from
    
       porting to Desktop Windows at Carolina Kiyoko Dell 
       at ckribeiro LAN back at Canal 2 - 
    
       at machine Dell at network NetVirtua 10Mbps at 
       processor i7 2670QM 8GB RAM 1 TB Disk 1.5TB Backup
       Samsung Disk with Windows 7 Home Premium
    
    ｛
    ｝
    
       ----
                            -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- appseed/app/app.cpp
+++ appseed/app/app.cpp
@@ -12,7 +12,14 @@ BEGIN_EXTERN_C
 int32_t WINAPI _tWinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int32_t nCmdShow)
 {
 
-   return app_core_main(hinstance, hPrevInstance, NULL, nCmdShow);
+   if(!defer_core_init())
+      return -1;
+
+   int iRet = app_core_main(hinstance, hPrevInstance, NULL, nCmdShow);
+
+   defer_core_term();
+
+   return iRet;
    
 }
 
--- appseed/app_core/app_core.cpp
+++ appseed/app_core/app_core.cpp
@@ -4,94 +4,9 @@
 CLASS_DECL_BASE int32_t __win_main(sp(::base::system) psystem,::windows::main_init_data * pmaininitdata);
 
 
-namespace base
-{
-
-   class teste
-   {
-   public:
-      teste()
-      {
-         output_debug_string("base::teste constructor\r\n");
-      }
-      ~teste()
-      {
-         output_debug_string("base::teste destructor\r\n");
-      }
-
-   };
-
-
-   class teste2: public teste
-   {
-   public:
-      teste2()
-      {
-         output_debug_string("base::teste2 constructor\r\n");
-      }
-      ~teste2()
-      {
-         output_debug_string("base::teste2 destructor\r\n");
-      }
-   };
-
-   class teste3: public teste2
-   {
-   public:
-      teste3()
-      {
-         output_debug_string("base::teste3 constructor\r\n");
-      }
-      virtual ~teste3()
-      {
-         output_debug_string("base::teste3 destructor\r\n");
-      }
-   };
-
-   class teste4: public teste3
-   {
-   public:
-      teste4()
-      {
-         output_debug_string("base::teste4 constructor\r\n");
-      }
-      virtual ~teste4()
-      {
-         output_debug_string("base::teste4 destructor\r\n");
-      }
-   };
-
-}
-
-
 
 extern "C" int32_t app_core_main(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int32_t nCmdShow)
 {
-
-   if(!defer_core_init())
-      return -1;
-
-   {
-      output_debug_string("----1\r\n");
-      output_debug_string("instanciando teste\r\n");
-      ::base::teste teste;
-   }
-   {
-      output_debug_string("----2\r\n");
-      output_debug_string("instanciando teste3\r\n");
-      ::base::teste2 teste3;
-   }
-   {
-      output_debug_string("----3\r\n");
-      output_debug_string("instanciando teste3\r\n");
-      ::base::teste3 teste;
-   }
-   {
-      output_debug_string("----4\r\n");
-      output_debug_string("instanciando teste4\r\n");
-      ::base::teste4 teste;
-   }
-
 
 
    UNREFERENCED_PARAMETER(lpCmdLine);
@@ -124,7 +39,6 @@ extern "C" int32_t app_core_main(HINSTANCE hinstance, HINSTANCE hPrevInstance, L
    }
 
    psystem = NULL;
-
 
    defer_core_term();
 
