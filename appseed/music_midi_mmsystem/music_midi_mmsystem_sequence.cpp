ef70b050397e61a0a063cf525d339e81348974a0
ca2 <camilo@ca2.email>
2014-07-21 02:45:25 +0000

    Mummi contribution to ca2 Desenvolvimento de Software self-image.
    Eu te amo Thommy Gustavo Borregaard Sørensen!!
    <3JegElskerDigThommy!!
    Mummi contribution!!!! Feliz 63 anos!! Graças a Deus!! Parabéns!!
    
    at inha at veriverse at ca2!!
    Dell XPS 8300 with Intel(R) Core(TM) i7-2600 CPU @ 3.4GHz 4 cores 8 threads 16GB RAM 500 GB x 2 HDD storage, 120 GB SSD OCZ Storage, 1TB x 2 Samsung Story Station Backup and Support HDD External Storage Windows 8.1 Pro at Alameda Princesa Izabel, 3000 Bigorrilho Curitiba PR Brazil
    from "Z:\ca2\" folder at inha-veriverse!!
    
    Remotely from
    
       porting to Desktop Windows at Carolina Kiyoko Dell 
       at ckribeiro LAN back at Canal 2 - 
    
       at machine Dell at network NetVirtua 10Mbps at 
       processor i7 2670QM 8GB RAM 1 TB Disk 1.5TB Backup
       Samsung Disk with Windows 7 Home Premium
    
    ｛
    ｝
    
       ----
                            -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- appseed/music_midi_mmsystem/music_midi_mmsystem_sequence.cpp
+++ appseed/music_midi_mmsystem/music_midi_mmsystem_sequence.cpp
@@ -186,7 +186,7 @@ Seq_Open_File_Cleanup:
             try
             {
 
-               file = Session.file().get_file(lpFileName, ::file::mode_read | ::file::share_deny_write | ::file::type_binary);
+               file = session().file().get_file(lpFileName, ::file::mode_read | ::file::share_deny_write | ::file::type_binary);
 
             }
             catch(...)
