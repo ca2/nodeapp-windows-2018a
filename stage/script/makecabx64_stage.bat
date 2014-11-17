5427c1e87cc4b5ba695c29dc40a5a7a22a115ab0
ca2 <camilo@ca2.email>
2014-11-17 00:18:28 +0000

    <3TBS Mummi contribution to ca2 Desenvolvimento de Software self-image.
    from Curitiba at Curitiba!!
    Eu te amo Thommy Gustavo Borregaard Sørensen!!
    
    Mummi contribution!!!! Feliz 63 anos!!
    
    <3JegElskerDigThommy!!
    Mummi contribution!!!! Feliz 63 anos!! Graças a Deus!! Parabéns!!
    
    at inha at veriverse at ca2!!
    Dell XPS 8300 with Intel(R) Core(TM) i7-2600 CPU @ 3.4GHz 4 cores 8 threads 16GB RAM 500 GB x 2 HDD storage, 120 GB SSD OCZ Storage, 1TB x 2 Samsung Story Station Backup and Support HDD External Storage Windows 8.1 Pro at Alameda Princesa Izabel, 3000 Bigorrilho Curitiba PR Brazil
    -8
    +8
    
    -8
    ????????????
    
       ??????????????!!
          ??????????????!!
       ??????????????!!
       ----
                            -   -   -
    Dziekuje Polska!!
    
       Thomas Borregaard Sørensen dziekuje!!
    
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    
    +8
    
    -8
    +8
    
    -8
    +8
    
    -8
    Mummi contribution!!, eu te amo demais!!
    +8
    
    -8
    +8
    
    8-8-88-8-8
    -----8nnnnnnnn8
    ++++++++++
    8+8+88+8+8
    
    -8
    +8
    
    -8
    +8

--- stage/script/makecabx64_stage.bat
+++ stage/script/makecabx64_stage.bat
@@ -31,8 +31,9 @@ copy ..\..\..\stage\x64\axismysql.dll ..\..\..\time\iexca2\x64\axismysql.dll
 copy ..\..\..\stage\x64\axisopenssl.dll ..\..\..\time\iexca2\x64\axisopenssl.dll
 copy ..\..\..\stage\x64\axiszlib.dll ..\..\..\time\iexca2\x64\axiszlib.dll
 copy ..\..\..\stage\x64\base.dll ..\..\..\time\iexca2\x64\base.dll
-copy ..\..\..\stage\x64\msvcp120d.dll ..\..\..\time\iexca2\x64\msvcp120d.dll
-copy ..\..\..\stage\x64\msvcr120d.dll ..\..\..\time\iexca2\x64\msvcr120d.dll
+copy ..\..\..\stage\x64\msvcp120.dll ..\..\..\time\iexca2\x64\msvcp120.dll
+copy ..\..\..\stage\x64\msvcr120.dll ..\..\..\time\iexca2\x64\msvcr120.dll
+copy ..\..\..\stage\x64\vcomp120.dll ..\..\..\time\iexca2\x64\vcomp120.dll
 copy ..\..\..\stage\x64\draw2d_gdiplus.dll ..\..\..\time\iexca2\x64\draw2d_gdiplus.dll
 copy ..\..\..\stage\x64\app.install.exe ..\..\..\time\iexca2\x64\app.install.exe
 ..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\iexca2.dll
@@ -47,8 +48,9 @@ copy ..\..\..\stage\x64\app.install.exe ..\..\..\time\iexca2\x64\app.install.exe
 ..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\axisopenssl.dll
 ..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\axiszlib.dll
 ..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\base.dll
-..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\msvcp120d.dll
-..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\msvcr120d.dll
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\msvcp120.dll
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\msvcr120.dll
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\vcomp120.dll
 ..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\draw2d_gdiplus.dll
 ..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\time\iexca2\x64\app.install.exe
 IF NOT EXIST ..\..\..\time\iexca2\x64\iexca2.cab ECHO Deleted iexca2.cab
