1b4830d1a880c3ddbba5d50b7957df0bfb1d857c
ca2 <camilo@ca2.email>
2015-02-10 02:52:52 +0000

    <3TBS Mummi contribution to ca2 Desenvolvimento de Software self-image.
    from Curitiba at Curitiba!!
    Eu te amo Thommy Gustavo Borregaard Sørensen!!
    
    Mummi contribution!!!! Feliz 63 anos!!
    
    <3JegElskerDigThommy!!
    Mummi contribution!!!! Feliz 63 anos!! Graças a Deus!! Parabéns!!
    
    at inha at veriverse at ca2!!
    Dell XPS 8300 with Intel(R) Core(TM) i7-2600 CPU @ 3.4GHz 4 cores 8 threads 16GB RAM 500 GB x 2 HDD storage, 120 GB SSD OCZ Storage, 1TB x 2 Samsung Story Station Backup and Support HDD External Storage Windows 8.1 Pro at Alameda Princesa Izabel, 3000 Bigorrilho Curitiba PR Brazil
    ｛
    ｝
    
       ----
                            -   -   -
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- appseed/draw2d_gdiplus/draw2d_gdiplus.cpp
+++ appseed/draw2d_gdiplus/draw2d_gdiplus.cpp
@@ -1,9 +1,5 @@
 #include "framework.h"
 
-Gdiplus::GdiplusStartupInput *   g_pgdiplusStartupInput     = NULL;
-Gdiplus::GdiplusStartupOutput *  g_pgdiplusStartupOutput    = NULL;
-DWORD_PTR                        g_gdiplusToken             = NULL;
-DWORD_PTR                        g_gdiplusHookToken         = NULL;
 
 BOOL WINAPI DllMain(HINSTANCE hInstance,DWORD dwReason,LPVOID lpReserved)
 {
@@ -24,76 +20,6 @@ BOOL WINAPI DllMain(HINSTANCE hInstance,DWORD dwReason,LPVOID lpReserved)
    if(dwReason == DLL_PROCESS_ATTACH)
    {
 
-      OutputDebugStringW(L"draw2d_gdiplus.dll initializing!\n");
-
-      xxdebug_box("draw2d_gdiplus.dll DllMain","box",MB_OK);
-      g_pgdiplusStartupInput     = new Gdiplus::GdiplusStartupInput();
-      g_pgdiplusStartupOutput    = new Gdiplus::GdiplusStartupOutput();
-      g_gdiplusToken             = NULL;
-      g_gdiplusHookToken         = NULL;
-
-      g_pgdiplusStartupInput->SuppressBackgroundThread = TRUE;
-
-      Gdiplus::Status statusStartup = GdiplusStartup(&g_gdiplusToken,g_pgdiplusStartupInput,g_pgdiplusStartupOutput);
-
-      if(statusStartup != Gdiplus::Ok)
-      {
-
-         simple_message_box(NULL,"Gdiplus Failed to Startup. ca cannot continue.","Gdiplus Failure",MB_ICONERROR);
-
-         return 0;
-
-      }
-
-
-
-
-      statusStartup = g_pgdiplusStartupOutput->NotificationHook(&g_gdiplusHookToken);
-
-
-      if(statusStartup != Gdiplus::Ok)
-      {
-
-      simple_message_box(NULL,"Gdiplus Failed to Hook. ca cannot continue.","Gdiplus Failure",MB_ICONERROR);
-
-      return 0;
-
-      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -101,13 +27,6 @@ BOOL WINAPI DllMain(HINSTANCE hInstance,DWORD dwReason,LPVOID lpReserved)
    else if(dwReason == DLL_PROCESS_DETACH)
    {
 
-      g_pgdiplusStartupOutput->NotificationUnhook(g_gdiplusHookToken);
-
-
-       ::Gdiplus::GdiplusShutdown(g_gdiplusToken);
-
-
-      OutputDebugStringW(L"draw2d_gdiplus.dll terminating!\n");
 
 
    }
