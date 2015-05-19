de3f1e0bdc80a0f60e33c1ce11ce0c70e6920d15
ca2 <camilo@ca2.email>
2015-05-19 07:50:35 +0000

    Mummi contribution to ca2 Desenvolvimento de Software self-image.
    from Curitiba at Curitiba!!
    Eu te amo Thommy Gustavo Borregaard Sørensen!!
    <3JegElskerDigThommy!!
    Mummi contribution!!!! Feliz 63 anos!! Graças a Deus!! Parabéns!!
    
    at inha at veriverse at ca2!!
    Dell XPS 8300 with Intel(R) Core(TM) i7-2600 CPU @ 3.4GHz 4 cores 8 threads 16GB RAM 500 GB x 2 HDD storage, 120 GB SSD OCZ Storage, 1TB x 2 Samsung Story Station Backup and Support HDD External Storage Windows 8.1 Pro at Alameda Princesa Izabel, 3000 Bigorrilho Curitiba PR Brazil
    from "Z:\ca2\" folder at inha-veriverse!!
    
    -8
    +8
    
    -8
    ????????????
    
       ??????????????!!
          ??????????????!!
       ??????????????!!
       ----
                            -   -   -
    Dziekuje Polska!!
    
       Thomas Borregaard Sørensen dziekuje!!
    
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    
    +8
    
    -8
    +8
    
    -8
    +8
    
    -8
    Mummi contribution!!, eu te amo demais!!
    +8
    
    -8
    +8
    
    8-8-88-8-8
    -----8nnnnnnnn8
    ++++++++++
    8+8+88+8+8
    
    -8
    +8
    
    -8
    +8
    
    <3TBS
    Mummi contribution to ca2 Desenvolvimento de Software self-image.
    
    from Curitiba at Curitiba!!
    Eu te amo Thommy Gustavo Borregaard Sørensen!!
    <3JegElskerDigThommy!!
    Mummi contribution!!!! Feliz 63 anos!! Graças a Deus!! Parabéns!!
    
    at inha at veriverse at ca2!!
    Dell XPS 8300 with Intel(R) Core(TM) i7-2600 CPU @ 3.4GHz 4 cores 8 threads 16GB RAM 500 GB x 2 HDD storage, 120 GB SSD OCZ Storage, 1TB x 2 Samsung Story Station Backup and Support HDD External Storage Windows 8.1 Pro at Alameda Princesa Izabel, 3000 Bigorrilho Curitiba PR Brazil
    from "Z:\ca2\" folder at inha-veriverse!!
    
    -8
    +8
    
    -8
    ????????????
    
       ??????????????!!
          ??????????????!!
       ??????????????!!
       ----
                            -   -   -
    Dziekuje Polska!!
    
       Thomas Borregaard Sørensen dziekuje!!
    
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    
    +8
    
    -8
    +8
    
    -8
    +8
    
    -8
    Mummi contribution!!, eu te amo demais!!
    +8
    
    -8
    +8
    
    8-8-88-8-8
    -----8nnnnnnnn8
    ++++++++++
    8+8+88+8+8
    
    -8
    +8
    
    -8
    +8

--- appseed/app_core/app_core.h
+++ appseed/app_core/app_core.h
@@ -3,16 +3,8 @@
 
 #include "aura/aura/aura.h"
 
-class app_core
-{
-public:
-
-
-   DWORD m_dwStartTime;
-   DWORD m_dwAfterApplicationFirstRequest;
-
-
-};
-
 
 extern "C" int32_t app_core_main(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int32_t nCmdShow, app_core & appcore);
+
+
+
