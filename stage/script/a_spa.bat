df215952d14980a7ffd1fb664d615d1a4ebab4fb
ca2 <camilo@ca2.email>
2015-06-03 04:04:20 +0000

    <3TBS Mummi contribution to ca2 Desenvolvimento de Software self-image.
    from Curitiba at Curitiba!!
    Eu te amo Thommy Gustavo Borregaard Sørensen!!
    <3JegElskerDigThommy!!
    Mummi contribution!!!! Feliz 63 anos!! Graças a Deus!! Parabéns!!
    
    at inha at veriverse at ca2!!
    Dell XPS 8300 with Intel(R) Core(TM) i7-2600 CPU @ 3.4GHz 4 cores 8 threads 16GB RAM 500 GB x 2 HDD storage, 120 GB SSD OCZ Storage, 1TB x 2 Samsung Story Station Backup and Support HDD External Storage Windows 8.1 Pro at Alameda Princesa Izabel, 3000 Bigorrilho Curitiba PR Brazil
    from "Z:\ca2\" folder at inha-veriverse!!
    
    -8
    +8
    
    -8
    ????????????
    
       ??????????????!!
          ??????????????!!
       ??????????????!!
       ----
                            -   -   -
    Dziekuje Polska!!
    
       Thomas Borregaard Sørensen dziekuje!!
    
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    
    +8
    
    -8
    +8
    
    -8
    +8
    
    -8
    Mummi contribution!!, eu te amo demais!!
    +8
    
    -8
    +8
    
    8-8-88-8-8
    -----8nnnnnnnn8
    ++++++++++
    8+8+88+8+8
    
    -8
    +8
    
    -8
    +8

--- stage/script/a_spa.bat
+++ stage/script/a_spa.bat
@@ -1,9 +1,9 @@
 @echo off
-..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\stage\x86\spa.exe
-..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\stage\x64\spa.exe
-..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\stage\x86\spaadmin.exe
-..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\stage\x64\spaadmin.exe
-copy ..\..\..\stage\x86\spa.exe Z:\netnodenet\net\netseed\front\ca2\_std\_std\exe\x86\spa.exe
-copy ..\..\..\stage\x64\spa.exe Z:\netnodenet\net\netseed\front\ca2\_std\_std\exe\x64\spa.exe
-copy ..\..\..\stage\x86\spaadmin.exe Z:\netnodenet\net\netseed\front\ca2\_std\_std\exe\x86\spaadmin.exe
-copy ..\..\..\stage\x64\spaadmin.exe Z:\netnodenet\net\netseed\front\ca2\_std\_std\exe\x64\spaadmin.exe
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\stage\x86\a_spa.exe
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\stage\x64\a_spa.exe
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\stage\x86\a_spaadmin.exe
+..\..\..\nodeapp\thirdparty\binary\signtool sign /f "X:\sensitive\sensitive\certificate\ca2.p12" /p none ..\..\..\stage\x64\a_spaadmin.exe
+copy ..\..\..\stage\x86\a_spa.exe Z:\netnodenet\net\netseed\front\ca2\_std\_std\exe\x86\a_spa.exe
+copy ..\..\..\stage\x64\a_spa.exe Z:\netnodenet\net\netseed\front\ca2\_std\_std\exe\x64\a_spa.exe
+copy ..\..\..\stage\x86\a_spaadmin.exe Z:\netnodenet\net\netseed\front\ca2\_std\_std\exe\x86\a_spaadmin.exe
+copy ..\..\..\stage\x64\a_spaadmin.exe Z:\netnodenet\net\netseed\front\ca2\_std\_std\exe\x64\a_spaadmin.exe
