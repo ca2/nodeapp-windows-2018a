6dc7e5ccf5443810c6bc4c49ad11a899bf1dd435
ca2 <camilo@ca2.email>
2015-09-19 08:54:22 +0000

    <3TBS!!
    Mummis!!

--- stage/dynamic_source/basis_libl64.bat
+++ stage/dynamic_source/basis_libl64.bat
@@ -1,3 +1,11 @@
-call "%VS_VARS%"  %VS_VARS_PLAT2%
+chcp 65001
+call "%VS_VARS%" %VS_VARS_PLAT2%
 
-link /OUT:"%CA2_ROOT%app\seed\metaseed\..\..\..\stage\%STAGEPLATFORM%\dynamic_source\%ITEM_NAME%.dll" /INCREMENTAL /NOLOGO /LIBPATH:"%CA2_ROOT%time/library/%PLATFORM%/%CONFIGURATION_NAME%/" /LIBPATH:"%CA2_ROOT%nodeapp/thirdparty/library/%PLATFORM%" /LIBPATH:"C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\lib\amd64" /LIBPATH:"C:\Program Files (x86)\Windows Kits\8.1\Lib\winv6.3\um\%PLATFORM%" /DLL "aura.lib" "axis.lib" "axisfreeimage.lib" "axisfreetype.lib"  "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" "base.lib" "core.lib" "netnode.lib" /DEF:"%CA2_ROOT%app-core/appseed/netnode_dynamic_source_library/netnode_dynamic_source_library.def" /MANIFEST:NO /ALLOW_ISOLATION /DEBUG /PDB:"%TARGET_PATH%.pdb" /SUBSYSTEM:WINDOWS   /TLBID:1 /DYNAMICBASE:NO /NXCOMPAT /IMPLIB:"%CA2_ROOT%app\seed\metaseed\../../../time/library/%PLATFORM%/%ITEM_NAME%.lib" /MACHINE:X64 /ERRORREPORT:QUEUE  "%CA2_ROOT%time\intermediate\%PLATFORM%\netnode_dynamic_source_library\framework.obj" "%CA2_ROOT%time\intermediate\%PLATFORM%\netnode_dynamic_source_library\netnode_dynamic_source_library.obj" %OBJS%  > "%CA2_ROOT%time/dynamic_source/%ITEM_NAME%-link-log.txt"
+
+
+
+
+link /OUT:"%TARGET_PATH%.dll" /MANIFEST:NO /NXCOMPAT /PDB:"%TARGET_PATH%.pdb" /DYNAMICBASE "aura.lib" "auracharguess.lib"  "aurasqlite.lib"  "axis.lib" "axisbzip2.lib" "axiscrypto.lib" "axisfreeimage.lib" "axisidn.lib" "axismysql.lib" "axisopenssl.lib" "axiszlib.lib" "base.lib" "core.lib" "data.lib" "html.lib" "html_lite.lib" "math.lib" "netnode.lib" "programming.lib" "time.lib" "userdatetime.lib" "app_core_alarm.lib" "aqua.lib" /DEBUG /FIXED:NO /DLL /MACHINE:X64 /INCREMENTAL:NO /SUBSYSTEM:WINDOWS  /ERRORREPORT:PROMPT /NOLOGO /LIBPATH:"%CA2_ROOT%time/library/x64/basis" /LIBPATH:"C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/lib/amd64" /LIBPATH:"C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/atlmfc/lib/amd64" /LIBPATH:"C:/Program Files (x86)/Windows Kits/10/lib/10.0.10150.0/ucrt/x64" /LIBPATH:"C:/Program Files (x86)/Windows Kits/8.1/lib/winv6.3/um/x64" /LIBPATH:"C:/Program Files (x86)/Windows Kits/NETFXSDK/4.6/Lib/um/x64" /LIBPATH:"%CA2_ROOT%time/x64/basis" /LIBPATH:"%CA2_ROOT%nodeapp/thirdparty/library/x64/basis" /TLBID:1    %OBJECTS% > "%CA2_ROOT%time/dynamic_source/%ITEM_NAME%-link-log.txt"
+
+
+
