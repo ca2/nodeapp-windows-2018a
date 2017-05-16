0db4dba6fe7933182562daf7025c0c3ca8009eb0
ca2 <camilo@ca2.email>
2017-05-16 20:28:39 +0000

    Flag!! Flying and Flaming Flag!!
    bilbo at veriverse
    i7 6700 32GB RAM Windows 10 Pro 2 x SSD 240 GB 2 TB HDD 
    (booting also macOS Sierra, Hyper-V and Ubuntu 16.04 Unity Numix Theme Numix Circle Icons)

--- appseed/draw2d_gdiplus/draw2d_gdiplus_graphics.cpp
+++ appseed/draw2d_gdiplus/draw2d_gdiplus_graphics.cpp
@@ -1419,6 +1419,13 @@ gdi_fallback:
    bool graphics::StretchBlt(int32_t xDst, int32_t yDst, int32_t nDstWidth, int32_t nDstHeight, ::draw2d::graphics * pgraphicsSrc, int32_t xSrc, int32_t ySrc, int32_t nSrcWidth, int32_t nSrcHeight, uint32_t dwRop)
    {
 
+      if (nDstWidth == nSrcWidth && nDstHeight == nSrcHeight)
+      {
+
+         return BitBlt(xDst, yDst, nDstWidth, nDstWidth, pgraphicsSrc, xSrc, ySrc, dwRop);
+
+      }
+
       if(pgraphicsSrc == NULL)
          return FALSE;
 
